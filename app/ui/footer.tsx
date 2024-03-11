78e8046eb16edacbfed4b1557c0e0c073b5f089e
jennaly <106183040+jennaly@users.noreply.github.com>
2024-03-11 11:38:19 -0700

    feat(ui): add footer component

---
+++ app/ui/footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "./logo";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="my-4 flex justify-between items-baseline font-medium text-classdrift-gray-200">
+      <div className="flex gap-1">
+        <span>@ 2022 </span>
+        <Logo />
+      </div>
+      <div className="flex gap-4">
+        <Link href={"/terms-of-service"}>Terms of Service</Link>
+        <Link href={"/terms-of-service"}>Privacy Policy</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
