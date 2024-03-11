a4667b037d243792f6503f4c5714e8892f6a1c9b
jennaly <106183040+jennaly@users.noreply.github.com>
2024-03-11 11:35:52 -0700

    chore(ui): import fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Space_Grotesk } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
