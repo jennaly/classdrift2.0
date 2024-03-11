7a24a39b3292851e94f8df510a815199dfb7e504
jennaly <106183040+jennaly@users.noreply.github.com>
2024-03-11 11:39:11 -0700

    feat(ui): add bottom banner component

---
+++ app/ui/bottomBanner.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import Description from "./description";
+import { Button } from "./button";
+
+const BottomBanner = () => {
+  return (
+    <div className="bg-black mt-10 flex px-8 pt-10 pb-14 lg:px-14 lg:py-10 rounded-xl flex-col lg:flex-row justify-center">
+      <Image
+        src={"/icons/Board.png"}
+        width={500}
+        height={500}
+        alt={"Doodle of a board"}
+        className="p-10 mx-auto lg:mx-0"
+      ></Image>
+      <div className=" text-white px-4 lg:px-10 my-auto flex flex-col gap-2  mx-auto lg:mx-0">
+        <p className="text-4xl lg:text-6xl font-semibold ">
+          Try it now,
+          <span className="text-classdrift-gray-200"> it&apos;s free</span>
+        </p>
+        <Description className="mt-6">
+          Streamline your messy teaching workflow and focus on what matters.
+        </Description>
+
+        <Button className="bg-classdrift-accent self-start">
+          Launch your workspace
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default BottomBanner;
