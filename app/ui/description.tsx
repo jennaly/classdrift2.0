d0834abdbf7bc85c6e6f2269a963ba8e1581e818
jennaly <106183040+jennaly@users.noreply.github.com>
2024-03-11 11:39:40 -0700

    feat(ui): create description text component

---
+++ app/ui/description.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+import React from "react";
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+const Description = ({ children, className }: Props) => {
+  return (
+    <p className={clsx("text-base font-normal leading-snug mb-4", className)}>
+      {children}
+    </p>
+  );
+};
+
+export default Description;
